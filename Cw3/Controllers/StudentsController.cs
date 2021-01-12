c2bd6123f33e137496f6ff1a78074baccf12d51b
s16449 <s16449@pjwstk.edu.pl>
2021-01-12 22:52:36 +0100

    complete

---
+++ Cw3/Controllers/StudentsController.cs
@@ -0,0 +1,66 @@
+﻿using Cw3.Models;
+using Cw3.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+
+namespace Cw3.Controllers
+{
+    [Route("api/students")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private readonly IDbService _dbService;
+
+        public StudentsController(IDbService dbService)
+        {
+            _dbService = dbService;
+        }
+
+        [HttpGet]
+        public IActionResult GetStudents(string orderBy)
+        {
+          
+            if(orderBy == "lastname")
+            {
+                return Ok(_dbService.GetStudents().OrderBy(s => s.LastName));
+            }
+
+
+            return Ok(_dbService.GetStudents());
+
+           // return $"Jan, Anna, Krzysztof sortowanie={orderBy}";
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id)
+        {
+            if(id == 1)
+            {
+                return Ok("Jan");
+            }
+            return NotFound("Student was not found");
+        }
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            student.IndexNumber = $"s{new Random().Next(1,200000)}";
+            return Ok(student);
+        }
+
+        [HttpPut("{id}")]
+
+        public IActionResult put(int id)            
+        {
+            return Ok("Zaktualizowano " + id);
+        }
+
+
+        [HttpDelete("{id}")]
+
+        public IActionResult delete(int id)
+        {
+            return Ok("Usunieto " + id);
+        }
+    }
+}
---
+++ Cw3/Services/MockDbService.cs
@@ -0,0 +1,27 @@
+﻿using Cw3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cw3.Services
+{
+    public class MockDbService : IDbService
+
+
+    {
+        private static IEnumerable<Student> _students = new List<Student>
+        {
+            new Student{IdStudent =1 , FirstName ="Anton", LastName="Antosiewicz", IndexNumber="s122321" },
+                new Student{IdStudent =2 , FirstName ="Waldek", LastName="Malewicz", IndexNumber="s352533" },
+                    new Student{IdStudent =3 , FirstName ="Pawel", LastName="Stefanczyk", IndexNumber="s435232" },
+                        new Student{IdStudent =4 , FirstName ="Przemek", LastName="Stanislawski", IndexNumber="s242332" }
+        };
+
+
+        public IEnumerable<Student> GetStudents()
+        {
+            return _students;
+        }
+    }
+}
